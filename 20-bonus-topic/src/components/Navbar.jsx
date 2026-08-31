03318fa1720587304d3f6be09a3ee7db2a8b010d
adarsh-seth <adarshseth999@gmail.com>
2026-09-01 02:25:22 +0530

    learned bonus topic

---
+++ 20-bonus-topic/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = (props) => {
+
+    function changeTheme(){
+        props.setTheme('Dark')
+    }
+    
+  return (
+    <div>
+        <button onClick={changeTheme}>Change Theme</button>
+    </div>
+  )
+}
+
+export default Navbar
